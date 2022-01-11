e8a8495652d0cfa0c03a995222865ee55e6ea144
Ryan D Reyes <ryan.reyes2012@gmail.com>
2022-01-10 17:49:02 -0700

    Adjusted Docker yml

--- lis/src/__init__.py
+++ lis/src/__init__.py
@@ -1,8 +1,7 @@
 import os
 from flask import Flask
 from flask_migrate import Migrate
-
-# from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 
 
 def create_app(test_config=None):
